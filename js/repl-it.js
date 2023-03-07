ccaef55db421016155dd66fa117bfa0b56cb9480
xiaozhushou-app <h23wfp60@duck.com>
2023-03-07 19:58:20 +0800

    format

--- js/repl-it.js
+++ js/repl-it.js
@@ -1,51 +1,51 @@
 // <div class="repl-it"></div>
 
 (() => {
+  let localStorage = window.localStorage || {getItem(){return []}, setItem(){}}
+  let history = JSON.parse(localStorage.getItem('repl-history') || '[]')
+  window.addEventListener('beforeunload', event => localStorage.setItem('repl-history', JSON.stringify(history)))
+
+  const KEY_LEFT = 37
+  const KEY_UP = 38
+  const KEY_RIGHT = 39
+  const KEY_DOWN = 40
+  const KEY_ENTER = 13
+
+  const get_ts = () => {
+    const date = new Date()
+    return [date.getHours(), date.getMinutes(), date.getSeconds()].map(v=>v.toString().padStart(2, '0')).join(':') + '.' + date.getMilliseconds()
+  }
+  const getCircularReplacer = () => {
+    const seen = new WeakSet()
+    return (key, value) => {
+      if (typeof value === 'object' && value !== null) {
+        if (seen.has(value)) return;
+        seen.add(value)
+      }
+      return value;
+    }
+  }
+  const appenLine = (textarea, line) => {
+    if (textarea.style.display != '') {
+        textarea.style.display = ''
+    } else {
+        textarea.value += '\n'
+    }
+    textarea.value += get_ts() + ' ' + line
+    let rows = textarea.value.split('\n').length
+    if (rows > 10) rows = 10;
+    textarea.rows = rows;
+    textarea.scrollBy(0, textarea.scrollHeight)
+  }
+
   const initRepl = () => {
     let div = document.querySelector('div.repl-it')
     if (!div) return;
     if (div['repl-inited'] == true) return; // alreay inited
     div['repl-inited'] = true
 
-    const KEY_LEFT = 37
-    const KEY_UP = 38
-    const KEY_RIGHT = 39
-    const KEY_DOWN = 40
-    const KEY_ENTER = 13
-
-    let localStorage = window.localStorage || {getItem(){return []}, setItem(){}}
-    let history = JSON.parse(localStorage.getItem('repl-history') || '[]')
-    window.onbeforeunload = ()=>localStorage.setItem('repl-history', JSON.stringify(history))
-
     let his_idx = null
     let cmp_cache = ''
-
-    const get_ts = () => {
-        const date = new Date()
-        return [date.getHours(), date.getMinutes(), date.getSeconds()].map(v=>v.toString().padStart(2, '0')).join(':') + '.' + date.getMilliseconds()
-    }
-    const getCircularReplacer = () => {
-      const seen = new WeakSet()
-      return (key, value) => {
-        if (typeof value === 'object' && value !== null) {
-          if (seen.has(value)) return;
-          seen.add(value)
-        }
-        return value;
-      }
-    }
-    const appenLine = (textarea, line) => {
-        if (textarea.style.display != '') {
-            textarea.style.display = ''
-        } else {
-            textarea.value += '\n'
-        }
-        textarea.value += get_ts() + ' ' + line
-        let rows = textarea.value.split('\n').length
-        if (rows > 10) rows = 10;
-        textarea.rows = rows;
-        textarea.scrollBy(0, textarea.scrollHeight)
-    }
 
     let writer = document.createElement('textarea')
     let reader = document.createElement('textarea')
