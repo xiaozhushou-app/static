e91d6b38386bb80f8e5233706d6fefe6b0ea946f
xiaozhushou-app <h23wfp60@duck.com>
2023-03-07 19:29:20 +0800

    support cmd history

--- js/repl-it.js
+++ js/repl-it.js
@@ -1,8 +1,23 @@
 // <div class="repl-it"></div>
 
-(()=> {
-  let div = document.querySelector('div[class="repl-it"]')
+const initRepl = () => {
+  let div = document.querySelector('div.repl-it')
   if (!div) return;
+  if (div['repl-inited'] == true) return; // alreay inited
+  div['repl-inited'] = true
+
+  const KEY_LEFT = 37
+  const KEY_UP = 38
+  const KEY_RIGHT = 39
+  const KEY_DOWN = 40
+  const KEY_ENTER = 13
+
+  let localStorage = window.localStorage || {getItem(){return []}, setItem(){}}
+  let history = JSON.parse(localStorage.getItem('repl-history') || '[]')
+  window.onbeforeunload = ()=>localStorage.setItem('repl-history', JSON.stringify(history))
+
+  let his_idx = null
+  let cmp_cache = ''
 
   const get_ts = () => {
       const date = new Date()
@@ -38,26 +53,60 @@
   writer.readOnly = true
   reader.rows = 1
   reader.placeholder = 'repl'
-  reader.onkeypress = event => {
-      let target = event.target
-      if (event.charCode == 13) {
-          let s = target.value.trim()
-
-          target.value = ''
-          event.preventDefault()
-          if (s == '') return;
-
-          appenLine(writer, s)
-
-          let res = ''
-          try {
-              res = eval(s)
-          } catch(error) {
-              res = error
-          }
-
-          appenLine(writer, res)
+  reader.onkeydown = event => {
+    if (history.length == 0) return;
+    switch (event.keyCode) {
+    case KEY_UP: {
+      if(his_idx == null) {
+        his_idx = -1
+        cmp_cache = event.target.value
+      } else {
+        if(his_idx <= - history.length) return;
+        his_idx -= 1
       }
+      break;
+    }
+    case KEY_DOWN: {
+      if(his_idx == null) return;
+      his_idx += 1;
+      if(his_idx > -1) his_idx = null;
+      break;
+    }
+    case KEY_ENTER: cmp_cache = ''; his_idx=null; return;
+    default: return;
+    }
+    event.preventDefault()
+
+    if (his_idx == null) {
+        event.target.value = cmp_cache
+        return;
+    }
+    const idx = history.length + his_idx
+
+    event.target.value = history[idx]
+
+  }
+  reader.onkeypress = event => {
+    let target = event.target
+    if (event.keyCode == 13) {
+        let s = target.value.trim()
+
+        target.value = ''
+        event.preventDefault()
+        if (s == '') return;
+
+        history = history.concat(s)
+        appenLine(writer, s)
+
+        let res = ''
+        try {
+            res = eval(s)
+        } catch(error) {
+            res = error
+        }
+
+        appenLine(writer, res)
+    }
   }
 
   div.appendChild(writer)
@@ -68,4 +117,7 @@
       console_log(...args)
       appenLine(writer, Array.from(args).map(v => JSON.stringify(v, getCircularReplacer())).join(' '))
   }
-})();
+}
+
+initRepl()
+new MutationObserver((record, observer) => initRepl()).observe(document.body, {subtree:true, childList:true})
